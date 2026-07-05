fcf26bf944c4291db4f5e87b652f27325153bec9
cooperdozier <cooper.dozier@gmail.com>
2026-07-05 19:30:24 -0400

    server working data not parsing

---
+++ color-schemes-js/index.js
@@ -0,0 +1,20 @@
+"use strict"
+
+async function getRandomColorScheme() {
+    const endpoint = "http://localhost:8080/api/v1/getRandomColorScheme";
+    try {
+        const response = await fetch(endpoint);
+        const returnedData = await response.json();
+        // const receivedScheme = returnedData.data;
+        // console.log(receivedScheme)
+        console.log(response)
+        // const imgDiv = document.querySelector(".background-img");
+        // imgDiv.style.backgroundImage = `url(${receivedPhotoUrl})`;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
+
+getRandomColorScheme();
