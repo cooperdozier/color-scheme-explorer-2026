a17665d7cb9e953f34fc5f57dffd9756d155ff12
cooperdozier <cooper.dozier@gmail.com>
2026-07-05 19:34:07 -0400

    try again

---
+++ color-schemes-js/js/explorer.js
@@ -0,0 +1,7 @@
+// Pseudocode
+
+// Watch button
+// Fetch scheme from colourlovers.com
+// Parse JSON
+// Add colors 1 by 1 to CSS with a map or a loop
+
---
+++ server.js
@@ -0,0 +1,48 @@
+"use strict"
+const express = require("express");
+const app = express();
+const port = 8080;
+const cors = require("cors");
+// const bodyParser = require('body-parser-xml');
+
+const corsOptions = {
+    origin: `http://localhost:${port}`
+}
+
+app.use(cors());
+app.use(express.static("./color-schemes-js"));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+// app.use(bodyParser({ limit: '100kb', xmlParseOptions: { explicitArray: false } }));
+
+
+async function getRandomColorScheme() {
+    const endpoint = `https://www.colourlovers.com/api/palettes/random?format=json`
+    try {
+        const response = await fetch(endpoint);
+        // return console.log(response);
+        const returnedData = await response.body.json();
+        const receivedScheme = returnedData.colors;
+        // return console.log(returnedData)
+        return console.log(receivedScheme);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// app.use('/api/v1/getRandomScheme', async (req, res) => {
+//   console.log(req.body); // Access the parsed XML object here
+//   res.send('XML received!');
+// });
+
+app.use("/api/v1/getRandomColorScheme", async (request, response) => {
+    response.status(200).json({
+        status: 200,
+        data: await getRandomColorScheme(),
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running http://localhost:${port}`);
+    console.log("Press Ctrl-C to end thi process.");
+})
